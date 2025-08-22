fc9b6ec6317fc27cb253376a55db6917f9bc0926
Archar1os <smithd0inelle@gmail.com>
2025-08-22 12:57:54 +0800

    added week11

---
+++ Midterms/WEEK11/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title:'The Ledger',
+            author: 'Smith Romero',
+            age:20,
+        };
+    }
+});
+app.mount("#app");
